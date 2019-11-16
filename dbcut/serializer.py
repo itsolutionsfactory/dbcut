ba36475c7cd3a78bdf8dc6d6a8e450832c1c0884
Salem Harrache <salem@harrache.info>
2019-11-16 08:44:00 +0100

    Removed ujson to improved json encoder compatibility

--- dbcut/serializer.py
+++ dbcut/serializer.py
@@ -1,17 +1,61 @@
 # -*- coding: utf-8 -*-
+import datetime
+import decimal
+import json
+import uuid
 from collections import OrderedDict
-
-import yaml
 from io import open
 
-try:
-    import ujson as json
-except ImportError:
-    import json
+import yaml
+from sqlalchemy.orm import Query
+
+from .utils import to_unicode
+
+
+class JSONEncoder(json.JSONEncoder):
+    """JSON Encoder class that handles conversion for a number of types not
+    supported by the default json library, especially the sqlalchemy objects.
+
+    :returns: object that can be converted to json
+    """
+
+    def default(self, obj):
+        if isinstance(obj, datetime.datetime):
+            representation = obj.isoformat()
+            if representation.endswith("+00:00"):
+                representation = representation[:-6] + "Z"
+            return to_unicode(representation)
+        if isinstance(obj, (datetime.date, datetime.time)):
+            return to_unicode(obj.isoformat())
+        elif isinstance(obj, (decimal.Decimal)):
+            return float(obj)
+        elif isinstance(obj, uuid.UUID):
+            return to_unicode(obj)
+        elif isinstance(obj, Query):
+            return list(obj)
+        elif isinstance(obj, bytes):
+            return obj.decode()
+        elif hasattr(obj, "tolist"):
+            return obj.tolist()
+        elif hasattr(obj, "to_dict"):
+            return obj.to_dict()
+        elif hasattr(obj, "__getitem__"):
+            try:
+                return dict(obj)
+            except Exception:
+                pass
+        elif hasattr(obj, "__iter__"):
+            return list(item for item in obj)
+        return super(JSONEncoder, self).default(obj)
 
 
 def to_json(data, **extra_kwargs):
-    kwargs = {"ensure_ascii": False, "indent": 2}
+    kwargs = {
+        "ensure_ascii": False,
+        "indent": 2,
+        "separators": (",", ": "),
+        "cls": JSONEncoder,
+    }
     kwargs.update(extra_kwargs)
     return json.dumps(data, **kwargs)
 
@@ -37,7 +81,8 @@ def represent_ordereddict(dumper, data):
 
         value.append((node_key, node_value))
 
-    return yaml.nodes.MappingNode(u'tag:yaml.org,2002:map', value)
+    return yaml.nodes.MappingNode(u"tag:yaml.org,2002:map", value)
+
 
 yaml.add_representer(OrderedDict, represent_ordereddict)
 
