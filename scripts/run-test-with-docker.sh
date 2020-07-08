dcafb463b0ee7204b405f22f156914b8a26ec70f
Salem Harrache <salem.harrache@itsfactory.fr>
2020-07-08 21:56:23 +0200

    Runtest tests on parallel with tmux xpanes

--- scripts/run-test-with-docker.sh
+++ scripts/run-test-with-docker.sh
@@ -4,20 +4,32 @@ ROOT_PROJECT="$(dirname "$(dirname "${BASH_SOURCE[0]}")")"
 export PYTHON_IMAGE=${PYTHON_IMAGE:-python:3.6}
 export POSTGRES_IMAGE=${POSTGRES_IMAGE:-postgres:9.6}
 export MYSQL_IMAGE=${MYSQL_IMAGE:-mariadb:10.3}
+export JOB_ID=$(echo ${TRAVIS_JOB_ID:-${CI_JOB_ID:-${TMUX_PANE:-"1"}}} | sed 's/[^0-9]*//g')
 
 echo "-------------------------------------------------------------------------------"
 echo ""
 echo "              python : ${PYTHON_IMAGE}"
 echo "            postgres : ${POSTGRES_IMAGE}"
 echo "               mysql : ${MYSQL_IMAGE}"
+echo "              job_id : ${JOB_ID}"
 echo ""
 echo "-------------------------------------------------------------------------------"
 
 cd $ROOT_PROJECT
 
+_print() { printf "\033[1;32m%b\033[0m\n" "$1"; }
 
-set -e
+_cleanup() {
+    _print ":: Cleanup"
+    docker-compose down -v --remove-orphans
+}
+
+trap _cleanup EXIT
+trap '{ exit 1; }' TERM INT
+
+
+_print ":: Building image"
 docker-compose build
-docker-compose down -v --remove-orphans 2> /dev/null
-docker-compose run --rm dbcut_app make test
-docker-compose down -v --remove-orphans 2> /dev/null
+
+_print ":: Running tests"
+docker-compose run --rm app make test
