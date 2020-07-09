5ea1d19e1d6815d7819fd205ea85c013b2811dba
Salem Harrache <salem.harrache@itsfactory.fr>
2020-07-09 08:45:25 +0200

    Set explicitly PROJECT_DIR from CI env or git

--- scripts/run-test-with-docker.sh
+++ scripts/run-test-with-docker.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 ROOT_PROJECT="$(dirname "$(dirname "${BASH_SOURCE[0]}")")"
 
+
 export PYTHON_IMAGE=${PYTHON_IMAGE:-python:3.6}
 export POSTGRES_IMAGE=${POSTGRES_IMAGE:-postgres:9.6}
 export MYSQL_IMAGE=${MYSQL_IMAGE:-mariadb:10.3}
+export PROJECT_DIR=${TRAVIS_BUILD_DIR:-${CI_PROJECT_DIR:-$(git rev-parse --show-toplevel)}}
 export JOB_ID=$(echo ${TRAVIS_JOB_ID:-${CI_JOB_ID:-${TMUX_PANE:-"1"}}} | sed 's/[^0-9]*//g')
 
 export COMPOSE_PROJECT_NAME=dbcut_test_${JOB_ID}
@@ -14,6 +16,7 @@ echo "              python : ${PYTHON_IMAGE}"
 echo "            postgres : ${POSTGRES_IMAGE}"
 echo "               mysql : ${MYSQL_IMAGE}"
 echo "              job_id : ${JOB_ID}"
+echo "         project dir : ${PROJECT_DIR}"
 echo ""
 echo "-------------------------------------------------------------------------------"
 
